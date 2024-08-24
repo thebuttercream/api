e40cf327b52f9ae384cb6c8a7b766777b6df9260
rikemorais <rikeaju@hotmail.com>
2024-08-24 16:48:17 -0300

    fix: Remove Auth

--- app/main.py
+++ app/main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
-from app.api.endpoints import auth, pdi, employee
+from app.api.endpoints import pdi, employee
 
 app = FastAPI()
 
-app.include_router(auth.router, prefix="/auth", tags=["auth"])
 app.include_router(pdi.router, prefix="/pdicollection", tags=["pdi"])
 app.include_router(employee.router, prefix="/employees", tags=["employee"])
 
