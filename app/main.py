9d9612f478801395e87d9a0c24c35c709d1463b8
rikemorais <rikeaju@hotmail.com>
2024-08-25 11:58:38 -0300

    feat: Adiciona Rota de Career

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-from app.api.endpoints import person
+from app.api.endpoints import person, career
 
 app = FastAPI()
 
+app.include_router(career.router, prefix="/career", tags=["career"])
 app.include_router(person.router, prefix="/person", tags=["person"])
